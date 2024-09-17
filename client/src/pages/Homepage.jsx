be3575ad25fd38129fe4a248ee7f4ae7a0d97193
jwc20 <choij2015@gmail.com>
2024-09-17 13:39:22 +0900

    Add react pages

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to the Homepage</h1>
+      </header>
+      <main>
+        <p>This is the homepage.</p>
+      </main>
+    </div>
+  );
+};
+
+export default Homepage;
