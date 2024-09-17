bdb4ce026a50e171e3cc0923124df0709e615c01
jwc20 <choij2015@gmail.com>
2024-09-17 18:00:22 +0900

    Minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::API
 
     include ActionController::Cookies
 
-
-    # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-    allow_browser versions: :modern
-
     # protect_from_forgery with: :exception
     # include SessionsHelper
     #     # Confirms a logged-in user.
