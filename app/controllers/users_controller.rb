711982808c29917f9369bab631f70c0d5e50f49e
jwc20 <choij2015@gmail.com>
2024-09-17 13:18:01 +0900

    Add user controls and configure routing and create helper for login sessions (use later)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
-end
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        log_in @user
+        redirect_to messages_url
+      else
+        render 'new'
+      end
+    end
+  
+    private
+  
+      def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+      end
+  end
+
