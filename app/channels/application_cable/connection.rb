9e3caf55a3b68e33e03732073ab09ad1624345b2
jwc20 <choij2015@gmail.com>
2024-09-17 13:39:07 +0900

    Check for users that are signed up

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,23 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include SessionsHelper
+
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private 
+
+    def find_verified_user
+      if logged_in?
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
   end
 end
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
+
